5c6bfcab88f1da5caa3ce1e8c8e04f354b3b9b45
tichise / Takuya Ichise <43707+tichise@users.noreply.github.com>
2020-08-29 14:05:34 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,15 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
-  name: "Harald"
+    name: "Harald",
+    products: [
+        .library(name: "Harald", targets: ["Harald"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "Harald", path: "Sources")
+    ],
+    swiftLanguageVersions: [.v5]
 )
